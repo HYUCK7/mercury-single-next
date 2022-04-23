67abafc54f1621f8b65d6616ed8665427c24a7f1
HYUCK7 <iqeq12191219@gmail.com>
2022-04-23 12:32:06 +0900

    0423 1232

--- pages/user/join.js
+++ pages/user/join.js
@@ -1,9 +1,11 @@
 import {useState} from 'react'
+import { useDispatch } from 'react-redux'
 
 export default function join(){
     const [user, setUser] = useState({
         userid: '', password: '', email: '', name: '', phone: '', birth: '', address: ''
     })
+    const dispatch = useDispatch()
     const handleChange = e => {
         e.preventdefault()
         const{name, value} = e.target
@@ -12,10 +14,38 @@ export default function join(){
     return <><h1>회원가입</h1>
     <form onSubmit={
         e => {
-            
+            e.preventDefault
+            alert('진행 1. 회원가입 클릭')
+            dispatch(userActions.joinRequest(user))
+            setUser({
+                userid: '', password: '', email: '', name: '', phone: '', birth: '', address: ''                
+            })
         }
     }>
+        <div>
+    <label><b>사용자ID</b></label>
+    <input type="text" name='userid' onChange={handleChange} /><br />
 
+    <label htmlFor=""><b>비밀번호</b></label>
+    <input type="text" name='password' onChange={handleChange}/><br />
+
+    <label htmlFor=""><b>이메일</b></label>
+    <input type="text" name='email' onChange={handleChange}/><br />
+
+    <label htmlFor=""><b>이름</b></label>
+    <input type="text" name='name' onChange={handleChange}/><br />
+
+    <label htmlFor=""><b>전화번호</b></label>
+    <input type="text" name='phone' onChange={handleChange}/><br />
+
+    <label htmlFor=""><b>생년월일</b></label>
+    <input type="text" name='birth' onChange={handleChange}/><br />
+
+    <label htmlFor=""><b>주소</b></label>
+    <input type="text" name='address' onChange={handleChange}/><br />
+
+    <button type="submit">회원가입</button><br />
+    </div>
     </form>
     </>
 }
